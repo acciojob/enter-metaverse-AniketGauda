55a840ea3b619ffdc7a20cab94dd737c35e27a8c
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-17 03:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let p = document.getElementById("status");
+let h = document.getElementById("hdr");
+let btn = document.getElementById("enterBtn");
+
+btn.onclick = (()=>{
+    p.setAttribute("style",`
+        display: none;`
+    );
+    h.setAttribute("style",`
+        display: block;`
+    );
+})
