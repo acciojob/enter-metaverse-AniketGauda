c63ded3cc62cfbee1f26a1325ef362e7d2b8f656
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-17 03:30:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-//your JS code here. If required.
 let p = document.getElementById("status");
-let h = document.getElementById("hdr");
 let btn = document.getElementById("enterBtn");
 
 btn.onclick = (()=>{
-    p.setAttribute("style",`
-        display: none;`
-    );
-    h.setAttribute("style",`
-        display: block;`
-    );
+    p.innerHTML = "<h1>Entered Metaverse</h1>";
 })
